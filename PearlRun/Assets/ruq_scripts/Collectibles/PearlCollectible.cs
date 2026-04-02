58144985c907565adfd3e56f625823f5ebaba0a9
Ruqaya <ruqayaahabib@gmail.com>
2026-04-02 23:07:18 +0300

    Score manager - Task 4
    Features added:
    - Pearl score tracking
    - Level timer
    - Grade calculation (S, A, B, C)
    - Level completion system
    - Integration with SaveSystem
    - Saving best score, best time, and best grade
    - Level completion save
    - Unlock next level after completion

--- PearlRun/Assets/ruq_scripts/Collectibles/PearlCollectible.cs
+++ PearlRun/Assets/ruq_scripts/Collectibles/PearlCollectible.cs
@@ -24,7 +24,36 @@ public class PearlCollectible : MonoBehaviour
 
         ApplyEffect();
 
+        if (ScoreManager.Instance != null)
+        {
+            ScoreManager.Instance.AddPearls(GetPearlScore());
+        }
+
         Destroy(gameObject);
+    }
+
+    private int GetPearlScore()
+    {
+        switch (pearlType)
+        {
+            case PearlType.White:
+                return 1;
+
+            case PearlType.Blue:
+                return 5;
+
+            case PearlType.Golden:
+                return 0;
+
+            case PearlType.Red:
+                return 0;
+
+            case PearlType.Qarqaoun:
+                return 3;
+
+            default:
+                return 0;
+        }
     }
 
     private void ApplyEffect()
@@ -32,23 +61,23 @@ public class PearlCollectible : MonoBehaviour
         switch (pearlType)
         {
             case PearlType.White:
-                GameManager.instance.AddScore(1);
+                Debug.Log("White pearl collected: +1 point");
                 break;
 
             case PearlType.Blue:
-                GameManager.instance.AddScore(5);
+                Debug.Log("Blue pearl collected: +5 points");
                 break;
 
             case PearlType.Golden:
-                GameManager.instance.RestoreLife(1);
+                Debug.Log("Golden pearl collected: restore life");
                 break;
 
             case PearlType.Red:
-                GameManager.instance.AddLife(1);
+                Debug.Log("Red pearl collected: extra life");
                 break;
 
             case PearlType.Qarqaoun:
-                GameManager.instance.AddScore(3);
+                Debug.Log("Qarqaoun sweet collected: +3 points");
                 break;
         }
     }
